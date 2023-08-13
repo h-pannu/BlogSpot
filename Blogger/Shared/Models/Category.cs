01aca33b5e1183fcd3070770361ccc7fe7e08f2f
Harpreet Pannu <er.harpreetpannu@live.com>
2023-08-13 15:36:12 -0700

    Added Models entities BlogPost,Category and User

---
+++ Blogger/Shared/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogger.Shared.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(200)]
+        public string Name { get; set; }
+        [Required, MaxLength(200)]
+        public string Slug { get; set; }
+    }
+}
---
+++ Blogger/Shared/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogger.Shared.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; }
+        [MaxLength(50)]
+        public string? LastName { get; set; }
+        [Required, MaxLength(100)]
+        public string Email { get; set; }
+        [Required, MaxLength(100)]
+        public string Salt { get; set; }
+        [Required, MaxLength(100)]
+        public string Hash { get; set; }
+
+    }
+}
