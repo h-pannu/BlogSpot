e46011ec29bbeafd5ad3340fd7fde5aad1a0f4f2
Harpreet Pannu <er.harpreetpannu@live.com>
2023-08-13 23:39:25 -0700

    Designed login page alongwith overlay while Login button is processing.

---
+++ Blogger/Shared/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogger.Shared.Models
+{
+    public class LoginModel
+    {
+        [Required, EmailAddress, DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+        [Required, MinLength(8)]
+        public string password { get; set; }
+    }
+}
