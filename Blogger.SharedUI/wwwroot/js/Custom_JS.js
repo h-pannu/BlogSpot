1535eca00e768dd89b660423695839232dd58f53
Harpreet Pannu <er.harpreetpannu@live.com>
2023-09-05 23:11:45 -0700

    Opening Modal to save category and hiding based on close button.

--- Blogger.SharedUI/wwwroot/js/Custom_JS.js
+++ Blogger.SharedUI/wwwroot/js/Custom_JS.js
@@ -1,3 +1,5 @@
 ﻿window.Alert = function (message) {
     alert(message);
-}
+};
+window.openModal = (id) => document.getElementById(id).showModal();
+window.closeModal = (id) => document.getElementById(id).close();
